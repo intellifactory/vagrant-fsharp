e208a5a05e5ae0f11b30c1c7e911620e8d5002ae
"Anton Tayanovskyy (t0yv0) ext:(%22) <anton.tayanovskyy@gmail.com>
2014-03-19 15:39:55 -0400

    Create arch-linux folder.

--- arch-linux/provision.sh
+++ arch-linux/provision.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 function i { sudo pacman -S --noconfirm $1; }
 sudo pacman -Syu --noconfirm
+i autoconf
+i automake
+i fakeroot
+i git
+i make
 i mono
 i monodevelop
+i pkg-config
 i xorg-server
 i xorg-server-utils
 i xorg-xinit
 i xterm
 i xfce4
 i xfce4-goodies
+#mkdir ~/fsharp-build
+#cd ~/fsharp-build
+#curl https://aur.archlinux.org/packages/fs/fsharp-git/PKGBUILD > PKGBUILD
+#makepkg
