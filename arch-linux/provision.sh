b52fc3e3a8b485ed4b4a990702f63557be00dd18
"Anton Tayanovskyy (t0yv0) ext:(%22) <anton.tayanovskyy@gmail.com>
2014-03-19 17:29:17 -0400

    Build F# on arch vm.

--- arch-linux/provision.sh
+++ arch-linux/provision.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 function i { sudo pacman -S --noconfirm $1; }
-sudo pacman -Syu --noconfirm
-i autoconf
-i automake
-i fakeroot
-i git
-i make
-i mono
-i monodevelop
-i pkg-config
-i xorg-server
-i xorg-server-utils
-i xorg-xinit
-i xterm
-i xfce4
-i xfce4-goodies
-#mkdir ~/fsharp-build
-#cd ~/fsharp-build
-#curl https://aur.archlinux.org/packages/fs/fsharp-git/PKGBUILD > PKGBUILD
-#makepkg
+
+function make_fsharp {
+    mkdir ~/fsharp-build
+    cd ~/fsharp-build
+    curl https://aur.archlinux.org/packages/fs/fsharp-git/PKGBUILD > PKGBUILD
+    makepkg && sudo pacman -U *.xz
+}
+
+function setupvm {
+    sudo pacman -Syu --noconfirm
+    i autoconf
+    i automake
+    i fakeroot
+    i git
+    i make
+    i mono
+    i monodevelop
+    i pkg-config
+    i xorg-server
+    i xorg-server-utils
+    i xorg-xinit
+    i xterm
+    i xfce4
+    i xfce4-goodies
+    make_fsharp
+}
+
+if [ -e "~/.setupvm" ]; then
+    setupvm
+    touch ~/.setupvm
+fi
